024c04709e9117934e7f009232247a30976b6213
y-kisse <kichise-yudai-vb@ynu.ac.jp>
2018-05-14 18:40:46 +0900

    [feat] EfficiencyMaxDatumクラス

---
+++ ECOLOG_Mobile_App/ECOLOG_Mobile_App/Models/EfficiencyMaxDatum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Realms;
+
+namespace ECOLOG_Mobile_App.Models
+{
+    public class EfficiencyMaxDatum: RealmObject
+    {
+        public int Torque { get; set; }
+        public int Rev { get; set; }
+        public int Efficiency { get; set; }
+    }
+}
