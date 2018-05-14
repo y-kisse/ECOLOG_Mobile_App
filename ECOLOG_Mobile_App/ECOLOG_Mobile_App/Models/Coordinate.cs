3f22f1ece7abdd3be50fe6b9daa7ff9dbb137917
y-kisse <kichise-yudai-vb@ynu.ac.jp>
2018-05-14 17:51:52 +0900

    [feat] Coordinateクラス

---
+++ ECOLOG_Mobile_App/ECOLOG_Mobile_App/Models/Coordinate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECOLOG_Mobile_App.Models
+{
+    public class Coordinate
+    {
+        public class Ynu
+        {
+            public static readonly double LatitudeStart = 35.47;
+            public static readonly double LatitudeEnd = 35.476;
+            public static readonly double LongitudeStart = 139.58;
+            public static readonly double LongitudeEnd = 139.60;
+        }
+
+        public class TommyHome
+        {
+            public static readonly double LatitudeStart = 35.43;
+            public static readonly double LatitudeEnd = 35.435;
+            public static readonly double LongitudeStart = 139.40;
+            public static readonly double LongitudeEnd = 139.42;
+        }
+    }
+}
