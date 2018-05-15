421abc2e48e544b3646c958d0eef1da4a89636bd
y-kisse <kichise-yudai-vb@ynu.ac.jp>
2018-05-15 13:46:32 +0900

    [feat] nav to datainsertion page

--- ECOLOG_Mobile_App/ECOLOG_Mobile_App/ViewModels/MainPageViewModel.cs
+++ ECOLOG_Mobile_App/ECOLOG_Mobile_App/ViewModels/MainPageViewModel.cs
@@ -5,15 +5,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace ECOLOG_Mobile_App.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
+        public ICommand NaviToDataInsertionPageCom { get; }
         public MainPageViewModel(INavigationService navigationService) 
             : base (navigationService)
         {
             Title = "Main Page";
+            _navigationService = navigationService;
+            NaviToDataInsertionPageCom = new DelegateCommand(() =>
+            {
+                _navigationService.NavigateAsync("DataInsertionPage");
+            });
         }
     }
 }
